fbc43673615fc891442d787e0d5c3b3e554310b3
evanezcent <pratamayo16@gmail.com>
2021-02-18 09:20:51 +0700

    add constant

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -1,4 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const colorPrimary = Color.fromRGBO(20, 43, 111, 0.9);
+const colorPrimary = Color(0xFF142B6F);
+const colorPrimary70 = Color(0xFF5B6B9B);
+const colorPrimary50 = Color(0xFF8A95B7);
+const colorPrimary30 = Color(0xFFB9C0D4);
+
+const colorSecondaryRed = Color(0xFFF05769);
+const colorSecondaryRed70 = Color(0xFFF58A96);
+const colorSecondaryRed50 = Color(0xFFF8ABB4);
+const colorSecondaryRed30 = Color(0xFFFBCDD2);
+
+const colorSecondaryYellow = Color(0xFFFFD602);
+const colorSecondaryYellow70 = Color(0xFFFFE34E);
+const colorSecondaryYellow50 = Color(0xFFFFEB81);
+const colorSecondaryYellow30 = Color(0xFFFFF3B4);
+
+const colorNeutral1 = Color(0xFFE1DEE5);
+const colorNeutral170 = Color(0xFFEAE8ED);
+const colorNeutral150 = Color(0xFFF0EFF2);
+const colorNeutral130 = Color(0xFFF6F6F8);
+
+const colorNeutral2 = Color(0xFFC4C4C4);
+
+const colorClear = Color(0xFF1BB55C);
+const colorError = Color(0xFFE74C3C);
+const colorBackground = Color(0xFFF9FBFB);
