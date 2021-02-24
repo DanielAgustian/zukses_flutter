8880829a36febe5ede2c2b224c8f053e9a9d17d4
danielagustian <danielagustianyoali@gmail.com>
2021-02-24 13:54:51 +0700

    task done

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Zukses: Application for Office',
       theme: ThemeData(
         fontFamily: 'Lato',
       ),
