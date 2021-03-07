01c43892305d95187a4c2788cdfcfdd81dab40c0
evanezcent <pratamayo16@gmail.com>
2021-03-07 19:39:36 +0700

    merge tama-dev

--- lib/main.dart
+++ lib/main.dart
@@ -31,10 +31,9 @@ void main() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   token = prefs.getString("token");
 
-  runApp(DevicePreview(
-      builder: (context) => MyApp(
-            token: token,
-          )));
+  runApp(MyApp(
+    token: token,
+  ));
 }
 
 class MyApp extends StatelessWidget {
