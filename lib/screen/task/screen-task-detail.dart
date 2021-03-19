9c6ba17c27062e06f91aa1c745e5e89ee4d8de43
evanezcent <pratamayo16@gmail.com>
2021-03-19 11:27:48 +0700

    comment done

--- lib/screen/task/screen-task-detail.dart
+++ lib/screen/task/screen-task-detail.dart
@@ -420,12 +420,14 @@ class _TaskDetailScreen extends State<TaskDetailScreen>
                     SizedBox(
                       height: 5,
                     ),
-                    Text(
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Senectus in nascetur massa aliquam sollicitudin tellus. Tincidunt tellus a hac aliquam pharetra, massa laoreet. Varius adipiscing at neque venenatis quam mattis dui odio mi. Vitae euismod blandit. (Note)",
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: colorNeutral3,
-                        )),
+                    Container(
+                      child: Text(
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Senectus in nascetur massa aliquam sollicitudin tellus. Tincidunt tellus a hac aliquam pharetra, massa laoreet. Varius adipiscing at neque venenatis quam mattis dui odio mi. Vitae euismod blandit. (Note)",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: colorNeutral3,
+                          )),
+                    ),
                     SizedBox(
                       height: 10,
                     ),
@@ -759,7 +761,7 @@ class _TaskDetailScreen extends State<TaskDetailScreen>
                       ],
                     ),
                     SizedBox(
-                      height: 5,
+                      height: 10,
                     ),
                     ListView.builder(
                       padding: EdgeInsets.all(0),
@@ -768,7 +770,9 @@ class _TaskDetailScreen extends State<TaskDetailScreen>
                       shrinkWrap: true,
                       itemBuilder: (context, index) {
                         return CommentBox(
-                            comment: "Lorem ipsum dolor sit amet, c",
+                            size: size,
+                            comment:
+                                "Lorem ipsum dolor sit amet, cboboa sl anapisna pna pindo aoad n[oa k doa a[ da[on a[oa [aomd [omfn lk  kh ahbd la[ß",
                             user: "Finley Khowira",
                             date: "14 Jan 2021");
                       },
