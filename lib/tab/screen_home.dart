257505700a855b3550be92f63a94a23619144e0e
evanezcent <pratamayo16@gmail.com>
2021-06-22 16:43:55 +0700

    null in task home

--- lib/tab/screen_home.dart
+++ lib/tab/screen_home.dart
@@ -138,8 +138,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
       body: WillPopScope(
         onWillPop: onWillPop,
         child: RefreshIndicator(
-          backgroundColor: colorPrimary70,
-          color: colorBackground,
+          color: colorPrimary,
           strokeWidth: 1,
           onRefresh: refreshData,
           child: Stack(
@@ -332,6 +331,9 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
           loading = false;
           tasks =
               state.task.length < 2 ? state.task : state.task.take(2).toList();
+        } else if (state is TaskPriorityStateFailLoad) {
+          loading = false;
+          tasks = [];
         }
 
         return Container(
@@ -348,20 +350,40 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                         col: 1,
                       ))
                 else
-                  ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    padding: EdgeInsets.all(1.0),
-                    itemCount: tasks.length,
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      return ListViewBox(
-                        title: tasks[index].taskName,
-                        detail: tasks[index].details,
-                        viewType: "task",
-                      );
-                    },
-                  ),
+                  tasks == null || tasks.length == 0
+                      ? Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Container(
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom: BorderSide(
+                                          width: 1, color: colorBorder))),
+                              height: 40,
+                              child: Center(
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 5),
+                                  child: Text("home_text20".tr(args: ["Task"]),
+                                      style: TextStyle(
+                                        color: colorPrimary,
+                                        fontSize: size.height < 569 ? 14 : 16,
+                                      )),
+                                ),
+                              )))
+                      : ListView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          padding: EdgeInsets.all(1.0),
+                          itemCount: tasks.length,
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return ListViewBox(
+                              title: tasks[index].taskName,
+                              detail: tasks[index].details,
+                              viewType: "task",
+                            );
+                          },
+                        ),
 
                 Padding(
                     padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
@@ -964,6 +986,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
           loading = true;
         } else {
           loading = false;
+          meetings = [];
         }
         return Container(
             margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
